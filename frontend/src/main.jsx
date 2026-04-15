ad73f308150db68410a671ba0dcd79cb6bcf9945
Yogarathnam S <yogarathnam.802367@sxcce.edu.in>
2026-04-15 13:58:57 +0530

    Fix Vercel deployment - configure for root path and fix API routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RepoAcademy">
+    <BrowserRouter basename="">
       <ThemeProvider>
         <App />
         <Toaster
