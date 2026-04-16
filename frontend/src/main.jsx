58d5e5096f078a8fd2daec472da4fd190dd2d678
Yogarathnam S <yogarathnam.802367@sxcce.edu.in>
2026-04-16 11:21:39 +0530

    Fix Vercel routing by removing GitHub Pages base path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RepoAcademy">
+    <BrowserRouter>
       <ThemeProvider>
         <App />
         <Toaster
